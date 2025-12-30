4ace893992cb964ce4ef236a55efa7d4943b2acf
Matviidev <matvii.shpyruk.dev@gmail.com>
2025-12-31 01:53:32 +0200

    feat: move origin to .env file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,11 +5,17 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+  const allowedOrigin = process.env.ALLOWED_ORIGIN || '';
+
+  if (allowedOrigin) {
+    app.enableCors({
+      origin: allowedOrigin,
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+    });
+  } else {
+    console.warn('CORS not enabled: no ALLOWED_ORIGIN set');
+  }
 
   app.useGlobalPipes(
     new ValidationPipe({
